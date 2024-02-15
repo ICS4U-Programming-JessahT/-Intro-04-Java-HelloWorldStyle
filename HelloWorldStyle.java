d0e9b03cd07c8db1456e186454d950b89d4f0406
Jessah Tumbaga <jessah.tumbaga@stu.ocsb.ca>
2024-02-15 15:33:03 +0000

    Addedv4 HelloWorldStyle.java

--- HelloWorldStyle.java
+++ HelloWorldStyle.java
@@ -1,24 +1,19 @@
 /**
-* Great self and to check if linter can run.
-*
-* @author Jessah
-* @version 1.0
-* @2024-02-09
-*/
-
+ * Great self and to check if linter can run.
+ *
+ * @author Jessah
+ * @version 1.0 @2024-02-09
+ */
 public final class HelloWorldStyle {
-    /**
-    * Private constructor to prevent instantiation of this utility class.
-    */
-    private HelloWorldStyle() {
-    }
+  /** Private constructor to prevent instantiation of this utility class. */
+  private HelloWorldStyle() {}
 
-    /**
-     * Print message.
-     *
-     * @param args Command-line arguments (not used in program).
-     */
-    public static void main(final String[] args) {
-        System.out.println("I'm not really sure what to write here.");
-    }
+  /**
+   * Print message.
+   *
+   * @param args Command-line arguments (not used in program).
+   */
+  public static void main(final String[] args) {
+    System.out.println("I'm not really sure what to write here.");
+  }
 }
